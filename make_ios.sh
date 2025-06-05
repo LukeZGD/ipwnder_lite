e5e930b01c6ca88ee4c5d9352cb631c70e9465f2
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2025-06-05 12:46:19 +0800

    Fix make ios

--- make_ios.sh
+++ make_ios.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# Built on macOS 10.15.7
-# The iPhoneOS 9.2 SDK must be present in /Applications/Xcode.app. I've put the entire Xcode 7.2 here; the fact that it's incompatible is irrelevant.
-# Xcode 11.7 is at /Applications/Xcode11.app. You can use a different version but it must be compatible with the system macOS.
+set -e
+# https://github.com/growtopiajaw/iPhoneOS-SDK/releases/download/v1.0/iPhoneOS9.2.sdk.zip
+# https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.11.sdk.tar.xz
+# extract and place in "SDKs" folder
 
 # This file was derived in part from https://danylokos.github.io/0x05/ as well as the files mk_iphoneos32.sh, mk_iphoneos64.sh, and mk_iphoneos_generic.sh present in this repo until fba4c52bfeaa44ea2a51cd89e0fc48a906d31d9d inclusive.
 
@@ -24,8 +25,8 @@ if [ -z "$(ls -A ./ra1npoc)" ]; then
 git submodule init && git submodule update
 fi
 
-IOS_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS9.2.sdk"
-MACOSX_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk"
+IOS_SDK="./SDKs/iPhoneOS9.2.sdk"
+MACOSX_SDK="./SDKs/MacOSX10.11.sdk"
 FRAMEWORKS="-framework IOKit -framework CoreFoundation"
 FLAGS="-Os -DDEBUG -DIPHONEOS_ARM -DApple_A6"
 LIBCURL="./lib/dynamic/iphoneos-arm/libcurl.dylib"
@@ -35,12 +36,10 @@ LIBCURL64R="./lib/dynamic/iphoneos-arm64-rootless/libcurl.dylib"
 FILES="main.c ra1npoc/src/common/common.c ra1npoc/src/io/iousb.c ra1npoc/src/exploit/checkm8_arm64.c src/exploit/limera1n.c src/exploit/s5l8950x.c src/common/payload.c src/common/usb_0xa1_2.c lib/partialzip/partial.c"
 
 # Prepare SDK for build (the iOS SDK doesn't have IOUSBLib.h)
-sudo cp -r $MACOSX_SDK/System/Library/Frameworks/IOKit.framework/Headers/ \
+cp -r $MACOSX_SDK/System/Library/Frameworks/IOKit.framework/Headers/ \
     $IOS_SDK/System/Library/Frameworks/IOKit.framework/Headers/ && \
-sudo cp $MACOSX_SDK/usr/include/libkern/OSTypes.h \
+cp $MACOSX_SDK/usr/include/libkern/OSTypes.h \
     $IOS_SDK/usr/include/libkern/ && \
-sudo xcode-select -s /Applications/Xcode11.app && \
-sudo xcodebuild -license accept && \
 rm -f -- ipwnder_iphoneos* && rm -f -- ipwnder_lite* && rm -f -- *.deb
 
 # Build 32-bit binary
@@ -55,22 +54,26 @@ strip ipwnder_iphoneos64 && ldid -S ipwnder_iphoneos64
 clang -isysroot $IOS_SDK $FILES -I./ra1npoc/src/include -I./include -lz $LIBCURL64R $FRAMEWORKS $FLAGS -DIPHONEOS_ARM64 -rpath /var/jb/usr/lib -arch arm64 -o ipwnder_lite64 && \
 strip ipwnder_lite64 && ldid -S ipwnder_lite64
 
-# Join them together
-lipo -create -output ipwnder_lite -arch armv7 ipwnder_iphoneos -arch arm64 ipwnder_iphoneos64 && rm -f -- ipwnder_iphoneos*
-
-ldid -Sent.xml ipwnder_lite
+ldid -Sent.xml ipwnder_iphoneos
+ldid -Sent.xml ipwnder_iphoneos64
 ldid -Sent.xml ipwnder_lite64
 
-# Create rootful DEB file
+# Create rootful 32-bit DEB file
 rm -rf -- package && mkdir -p package/usr/local/bin && \
-mv ipwnder_lite package/usr/local/bin/ipwnder_lite && \
+mv ipwnder_iphoneos package/usr/local/bin/ipwnder_lite && \
 mkdir package/DEBIAN && cp control package/DEBIAN/control && \
+find . -name ".DS_Store" -delete && dpkg-deb -Zgzip -b package && dpkg-name package.deb
+
+# Create rootful 64-bit DEB file (iOS 11+)
+rm -rf -- package && mkdir -p package/usr/local/bin && \
+mv ipwnder_iphoneos64 package/usr/local/bin/ipwnder_lite && \
+mkdir package/DEBIAN && cp control64 package/DEBIAN/control && \
 find . -name ".DS_Store" -delete && dpkg-deb -Zgzip -b package && dpkg-name package.deb
 
 # Create rootless DEB file
 rm -rf -- package && mkdir -p package/var/jb/usr/local/bin && \
 mv ipwnder_lite64 package/var/jb/usr/local/bin/ipwnder_lite && \
-mkdir package/DEBIAN && cp control64 package/DEBIAN/control && \
+mkdir package/DEBIAN && cp control64_rootless package/DEBIAN/control && \
 find . -name ".DS_Store" -delete && dpkg-deb -b package && dpkg-name package.deb
 
 # Remove package folder
